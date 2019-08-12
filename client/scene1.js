10d017974beed9b18a04c01d8c09b4a0168088db
NeedsAUsername <kevinchi118@gmail.com>
2019-08-12 12:46:23 -0500

    creates pacing method for characters and adds a snail enemy

--- client/scene1.js
+++ client/scene1.js
@@ -11,8 +11,9 @@ class Scene1 extends Phaser.Scene {
     this.load.image('SKY', 'assets/sky.png') 
     this.load.image('BACKGROUND', 'assets/maplestory_background.jpg')
     this.load.image('platform', 'assets/platform.png') 
-    this.load.spritesheet('platforms', 'assets/platforms1.png', {frameWidth: 80, frameHeight: 50})
     this.load.image('diamond', 'assets/diamond.png') 
+    this.load.spritesheet('snail', 'assets/snail.png', {frameWidth: 50, frameHeight: 35})
+    this.load.spritesheet('platforms', 'assets/platforms1.png', {frameWidth: 80, frameHeight: 50})
     this.load.spritesheet('woof', 'assets/woof.png', {frameWidth: 32, frameHeight: 32})
   }
 
@@ -52,6 +53,7 @@ class Scene1 extends Phaser.Scene {
     this.ledge = this.add.tileSprite(400, 470, 500, 35, 'platforms')
     this.addImmovablePhysics(this.ledge)
     this.platforms.add(this.ledge)
+
     this.ledge2 = this.add.tileSprite(600, 350, 300, 35, 'platforms')
     this.addImmovablePhysics(this.ledge2)
     this.platforms.add(this.ledge2)    
@@ -67,19 +69,28 @@ class Scene1 extends Phaser.Scene {
       diamond.body.gravity.y = 1000
     }
 
+    this.enemies = this.add.group() 
+    this.snail = this.enemies.create(500, this.platforms.children.entries[0].y - 40, 'snail') 
+    this.physics.add.existing(this.snail) 
+    this.snail.body.collideWorldBounds = true
+    this.snail.direction = 'left'
+
     this.score = 0
     this.scoreText = this.add.text(16, 16, 'Score: ' + this.score, {fontSize: '32px', fill: '#000'}) 
     this.cursors = this.input.keyboard.createCursorKeys()
 
+    console.log('platforms', this.platforms.children.entries[0].y)
     console.log('player', this.player)
     console.log('ground', this.ground)
     console.log('diamond', this.diamonds)
     console.log('scoreText', this.scoreText)
+    console.log('snail', this.snail)
   }
 
   update() {
     this.physics.collide(this.player, this.platforms)
     this.physics.collide(this.diamonds, this.platforms)
+    this.physics.collide(this.enemies, this.platforms)
     this.physics.overlap(this.player, this.diamonds, this.collectDiamond, null, this) 
 
     if (this.cursors.up.isDown && this.player.body.touching.down) {
@@ -102,6 +113,20 @@ class Scene1 extends Phaser.Scene {
       this.player.direction === 'left' ? this.player.setFrame(1) : this.player.setFrame(2)
     }
 
+
+    this.paceCharacter(this.snail, {speed: 40, distance: 200})
+    // const directionFactor = this.snail.direction === 'left' ? -1 : 1 
+    // this.snail.body.velocity.x = 40 * directionFactor 
+    // this.snail.distanceTraveled -= directionFactor 
+    // if (this.snail.distanceTraveled >= 200) {
+    //   this.snail.direction = 'right' 
+    // } 
+    // if (this.snail.distanceTraveled < -200) {
+    //   this.snail.direction = 'left'
+    // }
+
+    
+
     if (this.score >= 100) {
       this.winText = this.add.text(300, 16, 'You Win!', {fontSize: '64px', fill: '#000'}) 
       this.scene.pause()
@@ -121,5 +146,20 @@ class Scene1 extends Phaser.Scene {
     diamond.destroy()
     this.score += 10 
     this.scoreText.text = 'Score: ' + this.score 
+  }
+
+  paceCharacter(character, {speed, distance}) {
+    if (!character.distanceTraveled) { character.distanceTraveled = 0 }
+    const directionFactor = character.direction === 'left' ? -1 : 1 
+    character.body.velocity.x = speed * directionFactor 
+    character.distanceTraveled -= directionFactor
+    if (character.distanceTraveled >= distance) {
+      character.direction = 'right' 
+      character.flipX = true 
+    } 
+    if (character.distanceTraveled < -1 * distance) {
+      character.direction = 'left'
+      character.flipX = false
+    }
   }
 }
